85f0aa19a460f8cf78ea660d2a2dde9b200e1d0c
Fadil Ardiansyah <v@clude.id>
2021-01-09 13:12:07 +0700

    add new query switched

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -30,8 +30,8 @@ class DashboardController extends Controller
             $provider = new \Wohali\OAuth2\Client\Provider\Discord([
                 'clientId'          => '658613502415470631',
                 'clientSecret'      => 'D3XIPQD8dTHOm6scdmWS9pLkoW7fubtW',
-                // 'redirectUri'       => 'http://ec2-18-212-84-193.compute-1.amazonaws.com'
-                'redirectUri'       => 'http://localhost/kureha-log/public'
+                'redirectUri'       => 'http://ec2-18-212-84-193.compute-1.amazonaws.com'
+                // 'redirectUri'       => 'http://localhost/kureha-log/public'
 
             ]);
             
