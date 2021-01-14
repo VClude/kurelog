dc2e58321a7c5e110f5d8db19c0361d8e7111cb6
Fadil Ardiansyah <v@clude.id>
2021-01-14 15:01:22 +0700

    yaa

--- resources/views/grid.blade.php
+++ resources/views/grid.blade.php
@@ -38,8 +38,9 @@
                 @endphp
                 
                 @foreach ($imglist as $im)
-                    <img src="{{ asset($im) }}" data-toggle="tooltip" data-placement="bottom"  data-html="true"
+                    <a href="{{route('weapspec.define',[$uid,$ide,$gridlist[$b] ? $gridlist[$b] : 0])}}"><img src="{{ asset($im) }}" data-toggle="tooltip" data-placement="bottom"  data-html="true"
                     title="Colo Skill : <br> @if(isset($gridlist[$b])){{ $gridlist[$b] }} @else'Not found' @endif <br><br> Skill Desc : <br> {{ $ybe[$b] }} <br><br>   Colo Support : <br> @if(isset($cololist[$b])){{ $cololist[$b] }} @else'Not found' @endif "></img>
+                    </a>
                 @php
                     $b++;
                 @endphp
@@ -207,75 +208,7 @@
         <!-- END Row #4 -->
     </div>
 
-    <div class="row gutters-tiny js-appear-enabled animated fadeIn" data-toggle="appear">
-        <!-- Row #4 -->
-        <div class="col-md-12">
-            <div class="block block-themed block-mode-loading-inverse block-transparent bg-image w-100">
-            <div class="block-header">
-                    <h3 class="block-title">
-                        Weapon <small>Analyzer</small>
-                    </h3>
-                </div>
-                <div class="block-content block-content-full">
-                    <div class="row">
-                        <div class="col-md-2">
-                            <img src="http://localhost/kureha-log/public/assets/img/weapSR/CardS0335.png"
-                                data-toggle="tooltip" data-placement="bottom" data-html="true" title=""
-                                class="js-tooltip-enabled"
-                                data-original-title="Colo Skill : <br> Oblivion Staff's Healing Light (IV) Lv.16   <br><br> Skill Desc : <br> Massively restores HP to 1 ally. <br><br>   Colo Support : <br> Oblivion Staff's Recovery Support (I) Lv.16   ">
-                        </div>
-                        <div class="col-md-10">
-                            <div class="py-15 px-20 clearfix border-black-op-b">
-                                <div class="font-size-h3 font-w600 text-info js-count-to-enabled" data-toggle="countTo"
-                                    data-speed="1000" data-to="260">Orblivion Staff</div>
-                                <div class="font-size-sm font-w600 text-uppercase text-info-light">Blessed Gospel (III)
-                                    Lv.15</div>
-                                <div class="font-size-sm font-w600 text-uppercase text-info-light">Recovery Support (II)
-                                    Lv.15</div>
-                                    
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="row">-</div>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                        <div class="py-15 px-20 clearfix border-black-op-b">
-                          
-                          <div class="font-size-h3 font-w600 text-warning js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="260">5 times</div>
-                              <div class="font-size-sm font-w600 text-uppercase text-info-light">This weapon used in match</div>
-                          </div>
-
-                          <div class="py-15 px-20 clearfix border-black-op-b">
-                          
-                          <div class="font-size-h3 font-w600 text-warning js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="260">3/5 Times / 60%</div>
-                              <div class="font-size-sm font-w600 text-uppercase text-info-light">This weapon Does 2 Target</div>
-                          </div>
-
-                          <div class="py-15 px-20 clearfix border-black-op-b">
-                          
-                          <div class="font-size-h3 font-w600 text-warning js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="260">24,600</div>
-                              <div class="font-size-sm font-w600 text-uppercase text-info-light">Average Heals from this staff</div>
-                          </div>
-
-                          <div class="py-15 px-20 clearfix border-black-op-b">
-
-                            <canvas class="js-chartjs-lines chartjs-render-monitor" id="ujicoba"></canvas>
-                          
-                          </div>
-
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-        </div>
-        <!-- END Row #4 -->
-    </div>
+   
 
 </div>
 <!-- END Page Content -->
@@ -443,34 +376,7 @@
 
     $(document).ready(function(){
 
-        var ctx = document.getElementById('ujicoba');
-        var atkbuffchart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [1,2,3,4,5],
-                datasets: [{
-                    label: "Orblivion Staff Recover",
-                    fill: !0,
-                    backgroundColor: "rgba(156,204,101,.25)",
-                    borderColor: "rgba(156,204,101,1)",
-                    pointBackgroundColor: "rgba(156,204,101,1)",
-                    pointBorderColor: "#fff",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgba(66,165,245,1)",
-                data: [19343,26546,45432,18695,24586]
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-
+      
         // $.noConflict();
 // DataTable
 $('#btlog').DataTable({
