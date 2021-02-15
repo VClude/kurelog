ebb4d27a8e3aa7e252fbf5afebbb2249f5806e36
Fadil Ardiansyah <v@clude.id>
2021-02-16 05:05:21 +0700

    asd

--- resources/views/gc.blade.php
+++ resources/views/gc.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@flickr" />
+<meta name="twitter:title" content="Small Island Developing States Photo Submission" />
+<meta name="twitter:description" content="View the album on Flickr." />
+<meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
 <head>
     <title>GC 7 Rank</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
