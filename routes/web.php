9239836b5ecbbbad62b2f8dc7816c48d5ef58272
Fadil Ardiansyah <v@clude.id>
2021-02-16 05:16:36 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'App\Http\Controllers\DashboardController@index')->name('index');
-Route::get('/gc/{txt?}', 'App\Http\Controllers\DashboardController@gcView')->name('show.gc'); 
+Route::get('/gcrank/{txt?}', 'App\Http\Controllers\DashboardController@gcView')->name('show.gc'); 
 Route::get('/gclist/{txt?}', 'App\Http\Controllers\DashboardController@getGcRank')->name('get.gc');
 
 Route::group(['middleware' => 'page-cache'], function(){
