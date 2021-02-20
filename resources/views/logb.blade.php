0ce87985830e85ae4dab3a16e562d6deee3f40d6
Fadil Ardiansyah <v@clude.id>
2021-02-20 07:51:07 +0700

    ds

--- resources/views/logb.blade.php
+++ resources/views/logb.blade.php
@@ -3,7 +3,12 @@
 @section('content')
     <!-- Page Content -->
     <div class="content">
+    <div class="my-50 text-center">
+            
+                <h2 class="font-w700 text-white mb-10">Mencurry Battle Log Orang</h2>
+                <h3 class="h5 text-muted mb-0">dijampe jampe pake dukun biar gaul</h3>
 
+        </div>
 
         <div class="bg-pattern bg-black-op-25" style="background-image: url('assets/media/various/bg-pattern.png');">
             <div class="content text-center">
