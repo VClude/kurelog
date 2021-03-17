cf8587eeb5b7c84c9fcb0b6e0dd312b56f399822
Fadil Ardiansyah <v@clude.id>
2021-03-17 21:52:48 +0700

    ds

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2763,7 +2763,7 @@ class DashboardController extends Controller
                     $TS = "unknown";
                     break;
             }
-            $gain = $point3 == 0 ? 0 : $point3 - $point2;
+            $gain = $point4 == 0 ? 0 : $point4 - $point3;
 
             $data_arr[] = array(
                 "grank" => $id,
