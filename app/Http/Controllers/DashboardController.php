c4304707e5557669e620c5ed1b46f41f0856a757
Fadil Ardiansyah <v@clude.id>
2021-03-17 15:44:12 +0700

    ds

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -669,7 +669,7 @@ class DashboardController extends Controller
                         $data_arr[] = array(
                             "name" => $name,
                             "userId" => $userId,
-                            "job" => $CJ2,
+                            "CJ2" => $CJ2,
                             "hp" => $hp,
                             "cp" => $cp,
                             "lastlogin" => $lastlogin
