28cc9c67dc0ed63a1bff03e6aefce6af94ff77ec
Fadil Ardiansyah <v@clude.id>
2021-03-20 05:17:40 +0700

    ds

--- resources/views/gc.blade.php
+++ resources/views/gc.blade.php
@@ -22,6 +22,8 @@
         <h2 class="mb-4">GC 8 Rank (Last Update : {{$lu}} UTC)</h2>
         <h3>Reset Time : 05:00 UTC</h3>
         <p>Update on : 15:30 UTC / 22:30 GMT+7, 23:00 UTC / 06:00 GMT +7, 05:00 UTC / 12:00 GMT +7</p>
+        <h2 class="mb-4">Use Desktop view if you Browse this in Mobile for better view</h2>
+
         <label for="ts">Select TS:</label>
 
         <select name="ts" id="ts">
