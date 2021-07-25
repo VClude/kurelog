feb0308166154c8c8394eed3067637ddf8dc197e
Fadil Ardiansyah <v@clude.id>
2021-07-25 22:46:23 +0700

    ds

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
             <div class="col-md-6 col-xl-12">
                 <div class="block">
                     <div class="block-content">
-                        <h1>GC 12 Rank Data (Still Parsing)</h1>
+                        <h1>GC 12 Rank Data</h1>
 
                     </div>
                 </div>
@@ -36,7 +36,7 @@
                 <div class="block-content block-content-full">
                     <div class="py-15 px-20 clearfix border-black-op-b">
 
-                        <div class="font-size-sm font-w600 text-uppercase text-success-light"> GC 12 Finals</div>
+                        <div class="font-size-sm font-w600 text-uppercase text-success-light"> GC 12 Finals  (Still Parsing)</div>
 
                     </div>
 
