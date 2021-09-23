a5c06e93da2ceb682183f88ae72dc0226355fb30
xytuned <xytuned@gmail.com>
2021-09-23 18:37:43 +0700

    ds

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,18 +40,7 @@
         </div>
     </a>
     
-    <!-- <a href="{{ route('gc.twelve') }}">
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-xl-12">
-                <div class="block">
-                    <div class="block-content">
-                        <h1>GC 12 Finals Battle Log</h1>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a> -->
     
 
     
@@ -92,64 +81,6 @@
                         </a>
                         @endforeach
 
-
-
-
-            
-    <form>
-                     <input type="hidden" name="discord_id" value="{{$discordid}}" />
-                     <input type="hidden" name="discord_name" value="{{$discordname}}" />
-                    
-
-        
-        <div class="row gutters-tiny js-appear-enabled animated fadeIn" data-toggle="appear">
-            <div class="col-xl-12 d-flex align-items-stretch">
-                    <div class="block block-transparent bg-primary-dark d-flex align-items-center w-100">
-                        <div class="block-content block-content-full">
-                            <div class="py-15 px-20 clearfix border-black-op-b">
-
-                                <div class="font-size-sm font-w600 text-uppercase text-success-light"> GC 12 & 13 Finals Data</div>
-
-                                 <div class="font-size-sm font-w600 text-uppercase text-success"> GC 12 & 13 Finals data are pub now, click sign-up and refresh to see the data </div>
-                            </div>
-
-    <div class="py-15 px-20 clearfix border-black-op-b">
-        <a href="#" class="access-submit">
-            <div class="row justify-content-center">
-            <div class="col-md-6 col-xl-12">
-                <div class="block">
-                    
-                    <div class="block-content bg-gray js-tooltip-enabled">
-                        @if(count($isentry) == 0)
-                        <h1 class="textsign">Sign up</h1>
-                        @else
-                        <h1 class="textsign">Signed up</h1>
-                        @endif
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        </a>
-
-
-                                
-    </div>
-
-                            
-   </form>     
-                            
-                            
-                            
-                            
-
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-    @endif
 
 
 
